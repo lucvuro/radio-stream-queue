c65fb76973a570ce8dab12e4e41343c88d4d539f
Luc Vu <lucvuit@gmail.com>
2023-02-02 11:24:48 +0700

    off sleeping

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const StaticFilePlugin = require('@hapi/inert');
 const Routes = require('./routes');
 https = require('https')
 require('dotenv').config();
-setInterval(function() {
-    https.get(`${process.env.URLAWAKE}`)
-}, 300000); // every 5 minutes (300000)
-//Prevent sleeping
+// setInterval(function() {
+//     https.get(`${process.env.URLAWAKE}`)
+// }, 300000); // every 5 minutes (300000)
+// //Prevent sleeping
 async function startApp() {
 
   try {
